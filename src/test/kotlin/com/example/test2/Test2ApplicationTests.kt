c4aad68cc57f3597a54341f0b893a8db4a6bdce5
applepinepaprica <applepinepaprica@gmail.com>
2018-11-26 09:26:48 +0000

    Added test

--- src/test/kotlin/com/example/test2/Test2ApplicationTests.kt
+++ src/test/kotlin/com/example/test2/Test2ApplicationTests.kt
@@ -1,16 +1,31 @@
 package com.example.test2
 
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.junit4.SpringRunner
+import org.springframework.test.web.reactive.server.WebTestClient
+import org.springframework.web.reactive.function.server.RouterFunction
+import org.springframework.web.reactive.function.server.ServerResponse
 
 @RunWith(SpringRunner::class)
 @SpringBootTest
 class Test2ApplicationTests {
 
-	@Test
-	fun contextLoads() {
+	lateinit var client: WebTestClient
+
+	@Autowired
+	lateinit var routerFunction: RouterFunction<ServerResponse>
+
+	@Before
+	fun setUp() {
+		client = WebTestClient.bindToRouterFunction(routerFunction).build()
 	}
 
+	@Test
+	fun contextLoads() {
+		client.get().uri("/customer").exchange().expectStatus().isOk
+	}
 }
