78bb3b98476f514936587438d7c6133f3de0cfdd
applepinepaprica <applepinepaprica@gmail.com>
2018-11-26 09:29:35 +0000

    Sdeleuze's test

--- src/test/kotlin/com/example/test2/Test2ApplicationTests.kt
+++ src/test/kotlin/com/example/test2/Test2ApplicationTests.kt
@@ -1,31 +1,23 @@
 package com.example.test2
 
-import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.junit4.SpringRunner
 import org.springframework.test.web.reactive.server.WebTestClient
-import org.springframework.web.reactive.function.server.RouterFunction
-import org.springframework.web.reactive.function.server.ServerResponse
 
 @RunWith(SpringRunner::class)
 @SpringBootTest
+@AutoConfigureWebTestClient
 class Test2ApplicationTests {
 
-	lateinit var client: WebTestClient
-
 	@Autowired
-	lateinit var routerFunction: RouterFunction<ServerResponse>
-
-	@Before
-	fun setUp() {
-		client = WebTestClient.bindToRouterFunction(routerFunction).build()
-	}
+	private lateinit var client: WebTestClient
 
 	@Test
 	fun contextLoads() {
-		client.get().uri("/customer").exchange().expectStatus().isOk
+		client.get().uri("/").exchange().expectStatus().isOk
 	}
 }
